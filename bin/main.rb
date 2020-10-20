5d257b3c115b5a4eaeab120c0fe797add42b8af7
jhdezj <jorgehdezj18@gmail.com>
2020-10-20 14:27:11 -0500

    add game winning cases

--- bin/main.rb
+++ bin/main.rb
@@ -1,57 +1,63 @@
-# #!/usr/bin/env ruby
+#!/usr/bin/env ruby
 
-#puts "Hello World!"
+require '../lib/player.rb'
 
+winner_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7]]
 winner = false
 draw = false
-validator = false
-spots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-puts "Enter name for the first player"
-player1 = "player1"
-puts "Enter name for the second player"
-player2 = "player2"
+
+spots = %w[1 2 3 4 5 6 7 8 9]
+puts 'Enter name for the first player'
+name = gets.chomp
+player1 = Player.new(name, 'x', 1)
+puts 'Enter name for the second player'
+name = gets.chomp
+player2 = Player.new(name, 'o', 2)
 
 board = "\n[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]\n"
 
 puts board
 
+validator = false
+
+current_player = player1
+
 while winner == false || draw == false
 
+
   unless  validator
+    puts "(#{current_player.name}) choose your move :"
+    player_input = gets.chomp
 
-    puts "(#{player1}) choose your move :"
-    p1 = gets.chomp
-    if spots.include?(p1)
-    spots.delete(p1)
-    validator = true
-    print spots
-    board = board.gsub(p1, "x")
- puts board
-
-    else 
-    #validator = true if p1 == /[1-9]/
-    puts "Enter a valid spot number: "
-    end
-  end
-validator = false
-  unless  validator
-    puts "(#{player2}) choose your move :"
-    p2 = gets.chomp
-    if spots.include?(p2) 
-    spots.delete(p2)
-    validator = true
-    print spots
-    board = board.gsub(p2, "o")
- puts board
-
-    else 
-    #validator = true if p1 == /[1-9]/
-    puts "Enter a valid spot number: "
+    if spots.include?(player_input)
+      spots.delete(player_input)
+      validator = true
+      print spots
+      board = board.gsub(player_input, current_player.symbol)
+      puts board
+    else
+      puts 'Enter a valid spot number: '
+      next
     end
   end
 
+  if current_player == player1
+    current_player = player2
+  elsif current_player == player2
+    current_player = player1
+  end
 
+  validator = false
 
-validator= false
+  draw = true if spots == []
+  
+  winner_cases.each |case|
+    winner = true if current_player.mark_spots.include?(case)
+  end  
+  
+
+  
+
 
 end
+
--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :name, :symbol, :turn
+  attr_accessor :name, :symbol, :turn, :mark_spots
   def initialize(name, symbol, turn)
     @name = name
     @symbol = symbol
     @turn = turn
+    @mark_spots =[]
+  end
+
+  def add_mark(mark)
+    @mark_spots=@mark_spots.push(mark)
   end
 end
+
+
+winner_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7]]
+test_player= Player.new("Test_name", "o", 1)
+
+test_player.add_mark(1)
+print test_player.mark_spots
+print "\n"
+test_player.add_mark(2)
+print test_player.mark_spots
+test_player.add_mark(3)
+print test_player.mark_spots
+[1,2,3] indclue? [1,2,3] ---false
+
+winner_cases.each  do |x| 
+puts "winner!" if test_player.mark_spots.include?(x)
+end  
