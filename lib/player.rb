65dda9cdadb03dad61972cfaa52e948166d651db
jhdezj <jorgehdezj18@gmail.com>
2020-10-20 15:34:54 -0500

    add winning cases logic

--- lib/player.rb
+++ lib/player.rb
@@ -6,27 +6,10 @@ class Player
     @name = name
     @symbol = symbol
     @turn = turn
-    @mark_spots =[]
+    @mark_spots = []
   end
 
   def add_mark(mark)
-    @mark_spots=@mark_spots.push(mark)
+    @mark_spots = @mark_spots.push(mark)
   end
 end
-
-
-winner_cases = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7]]
-test_player= Player.new("Test_name", "o", 1)
-
-test_player.add_mark(1)
-print test_player.mark_spots
-print "\n"
-test_player.add_mark(2)
-print test_player.mark_spots
-test_player.add_mark(3)
-print test_player.mark_spots
-[1,2,3] indclue? [1,2,3] ---false
-
-winner_cases.each  do |x| 
-puts "winner!" if test_player.mark_spots.include?(x)
-end  
