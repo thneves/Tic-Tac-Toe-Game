46ddd12432f294acfe8574ab3efe1bc1ba281189
jhdezj <jorgehdezj18@gmail.com>
2020-10-22 06:32:34 -0500

    add default names in case of empty name imputs

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,8 @@ class Player
   attr_accessor :name, :mark_spots, :current_player
   attr_reader :symbol, :turn
   def initialize(name, symbol, turn)
-    @name = name
+    @name = "Player #{turn}" if name == ''
+    @name = name if name != ''
     @symbol = symbol
     @turn = turn
     @mark_spots = []
