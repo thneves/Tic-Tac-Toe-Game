f0c32a0f8e02597fb9feb85c16d5e70862966954
thneves <tsneves11@gmail.com>
2020-10-27 13:32:46 -0300

    fix rubocop errors

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,7 @@
 require './lib/player.rb'
 
 describe Player do
-  let(:player1) { Player.new('player1', 'x', 1 )}
+  let(:player1) { Player.new('player1', 'x', 1) }
 
   describe '#add_mark' do
     it 'push mark into an array' do
